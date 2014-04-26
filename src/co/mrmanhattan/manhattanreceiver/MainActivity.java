283363720151be31cc374f37dc1d21638181098b
Hampus Ahlgren <hampus@qudini.com>
2014-04-26 18:19:04 +0100

    Added stuff

--- src/co/mrmanhattan/manhattanreceiver/MainActivity.java
+++ src/co/mrmanhattan/manhattanreceiver/MainActivity.java
@@ -1,15 +1,39 @@
 package co.mrmanhattan.manhattanreceiver;
 
+import java.util.Set;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
+// TODO: Get a list of all the available bluetooth devices and show them in a list
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ArrayAdapter<String> devicesArray = new ArrayAdapter<String>(this, R.id.secondLine);
+		BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
+		
+		// check if the bluetooth is enabled
+		if( !bluetooth.isEnabled() ){
+			Intent startBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(startBluetooth, 1);
+		}else{
+			
+			Set<BluetoothDevice> devices = bluetooth.getBondedDevices();
+			
+			for (BluetoothDevice device : devices) {
+				devicesArray.add(device.getName() + "\n" + device.getAddress());
+			}
+		}
+				
 	}
 
 	
