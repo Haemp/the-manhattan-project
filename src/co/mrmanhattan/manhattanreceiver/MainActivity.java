676f00c4fdd31ca7c1be52730f9fa6539874f15a
Hampus Ahlgren <hampus@qudini.com>
2014-04-27 20:26:02 +0100

    State: complete reading from byte array and displaying the last value

--- src/co/mrmanhattan/manhattanreceiver/MainActivity.java
+++ src/co/mrmanhattan/manhattanreceiver/MainActivity.java
@@ -1,10 +1,17 @@
 package co.mrmanhattan.manhattanreceiver;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Set;
 
+import javax.crypto.AEADBadTagException;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.ParcelUuid;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
@@ -14,6 +21,7 @@ import android.content.Intent;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 // TODO: Get a list of all the available bluetooth devices and show them in a list
 public class MainActivity extends Activity {
@@ -21,13 +29,28 @@ public class MainActivity extends Activity {
 	private ArrayList<String> deviceNames;
 	private BluetoothAdapter bluetooth; 
 	private ConnectThread thread;
+	public TextView text;
+	
+	Handler mHandler = new Handler(){
+		
+		@Override
+	    public void handleMessage(Message msg) {
+			byte[] a = (byte[]) msg.obj;
+			byte b = a[0];
+			String txt = b+"";
+			text.setText(txt);
+			
+			System.out.println("Message" +msg.toString());
+		}
+	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		this.deviceNames = new ArrayList<String>();
-		
+		text = (TextView) findViewById(R.id.signal);
+		text.setText("Initated");
 		bluetooth = BluetoothAdapter.getDefaultAdapter();
 		BluetoothSocket socket = null;
 		// check if the bluetooth is enabled
@@ -36,7 +59,7 @@ public class MainActivity extends Activity {
 		// send this device to the thread to handle the 
 		// socket connection
 		this.thread = new ConnectThread(device);
-		this.thread.run();
+		this.thread.start();
 	}
 	
 	
@@ -112,10 +135,75 @@ public class MainActivity extends Activity {
 	        }
 	 
 	        // Do work to manage the connection (in a separate thread)
-	        //manageConnectedSocket(mmSocket);
+	        Thread cThread = new ConnectedThread(mmSocket);
+	        cThread.start();
+	        
 	    }
 	 
 	    /** Will cancel an in-progress connection, and close the socket */
+	    public void cancel() {
+	        try {
+	            mmSocket.close();
+	        } catch (IOException e) { }
+	    }
+	}
+	
+	
+	
+	
+	private class ConnectedThread extends Thread {
+	    private final BluetoothSocket mmSocket;
+	    private final InputStream mmInStream;
+	    private final OutputStream mmOutStream;
+	 
+	    public ConnectedThread(BluetoothSocket socket) {
+	        mmSocket = socket;
+	        InputStream tmpIn = null;
+	        OutputStream tmpOut = null;
+	 
+	        // Get the input and output streams, using temp objects because
+	        // member streams are final
+	        try {
+	            tmpIn = socket.getInputStream();
+	            tmpOut = socket.getOutputStream();
+	        } catch (IOException e) { }
+	 
+	        mmInStream = tmpIn;
+	        mmOutStream = tmpOut;
+	    }
+	 
+	    public void run() {
+	        byte[] buffer = new byte[1024];  // buffer store for the stream
+	        int bytes; // bytes returned from read()
+	        
+	        // Keep listening to the InputStream until an exception occurs
+	        while (true) {
+	            try {
+	                // Read from the InputStream
+	            	Thread.sleep(1);
+	                bytes = mmInStream.read(buffer);
+	                
+	                // Send the obtained bytes to the UI activity
+	                mHandler.sendMessage(Message.obtain(mHandler, 9001, buffer));
+	                // mHandler.obtainMessage(9999, bytes)
+	                //       .sendToTarget();
+	            } catch (IOException e) {
+	                break;
+	            } catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        }
+	    }
+	 
+	    /* Call this from the main activity to send data to the remote device */
+	    public void write(byte[] bytes) {
+	        try {
+	            mmOutStream.write(bytes);
+	        } catch (IOException e) { }
+	    }
+	 
+	    /* Call this from the main activity to shutdown the connection */
 	    public void cancel() {
 	        try {
 	            mmSocket.close();
